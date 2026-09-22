9b3fed08fcb9ff4a0c058f20f24b4bce61a06787
WhatYouThing <whatuthing@gmail.com>
2026-09-22 22:04:53 +0200

    save config on main screen close

--- src/main/java/ing/whatyouth/noproxymod/MainScreen.java
+++ src/main/java/ing/whatyouth/noproxymod/MainScreen.java
@@ -10,6 +10,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import nofrills.config.Config;
 import nofrills.hud.clickgui.Settings;
 import nofrills.hud.clickgui.components.PlainLabel;
 import nofrills.misc.RenderColor;
@@ -119,6 +120,7 @@ public class MainScreen extends Settings {
 
     @Override
     public void onClose() {
+        Config.save();
         this.minecraft.setScreen(this.previous);
     }
 }
